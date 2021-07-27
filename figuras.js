0be54360f88dc879f9689b63edafe69d94304f68
Daniel Marin <danielmarin998@hotmail.com>
2021-07-27 17:13:48 -0500

    Se añadio el reto del taller 1 como comentario.

--- figuras.js
+++ figuras.js
@@ -22,6 +22,19 @@ function areaTriangulo(base, altura){
     return (base * altura)/2;
 }
 
+// Altura triangulo 
+
+// function altura(lado1, lado2, base){
+//     if(lado1 === lado2 && lado1 != base){
+//         alert('isoceles')
+//         const altura = Math.sqrt(lado1**2 - base**2/ 4)
+//         console.log(altura)
+//     }
+//     else{
+//         alert('no lo se')
+//     }
+// }
+
 console.groupEnd();
 
 // Codigo del circulo
